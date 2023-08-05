09faaee72d5bb6d0652c5a89bf2cf1769075449f
Deadprogremer <106425760+Deadprogremer@users.noreply.github.com>
2023-08-05 22:04:20 +0500

    Add files via upload

---
+++ HTML/web site/index.js
@@ -0,0 +1,23 @@
+function header(){
+    document.getElementById("nav").style.display="block";
+    document.querySelector(".menu").style.display="none";
+}
+function cancel(){
+    document.getElementById("nav").style.display="none";
+    document.querySelector(".menu").style.display="block";
+}
+function change(){
+    document.getElementById("img1").style.display="block";
+    document.getElementById("img2").style.display="none";
+    document.getElementById("img3").style.display="none";
+}
+function change1(){
+    document.getElementById("img1").style.display="none";
+    document.getElementById("img2").style.display="block";
+    document.getElementById("img3").style.display="none";
+}
+function change2(){
+    document.getElementById("img1").style.display="none";
+    document.getElementById("img2").style.display="none";
+    document.getElementById("img3").style.display="block";
+}
